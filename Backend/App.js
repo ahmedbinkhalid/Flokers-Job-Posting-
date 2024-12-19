5cf815dac64c8a8181468610598506a88769a031
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2024-12-19 14:18:16 +0500

    Setup

---
+++ Backend/App.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(cookieParser());
+app.use(cors());
+
+app.use(bodyParser.json({limit: '10mb'}));
+app.use(bodyParser.urlencoded({limit: '10mb' , extended: true}));
+
+app.use((req, res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
